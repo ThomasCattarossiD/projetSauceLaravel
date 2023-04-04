c446b7412165d0aca9d34ad41b239e5fe443675d
Thomas CATTAROSSI DARTIGUELONGUE <pro.thomascd@gmail.com>
2023-04-04 09:36:49 +0200

    ajout du dossier vendor

--- database/migrations/2023_04_03_140536_create_sauces_table.php
+++ database/migrations/2023_04_03_140536_create_sauces_table.php
@@ -28,7 +28,6 @@ return new class extends Migration
             $table->foreign("userId")
             ->references("idUtilisateur")
             ->on("utilisateurs")
-            ->onDelete("cascade");
 
             $table->timestamps();
         });
